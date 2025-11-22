c3f77434d2f275ac6783fd4d1479d53e1542192c
Ryan Hoffer <ryanlhoffer@gmail.com>
2025-11-22 15:28:35 -0500

    terraform config

---
+++ main.tf
@@ -0,0 +1,19 @@
+# main.tf
+terraform {
+  required_version = ">= 1.0"
+}
+
+# A simple null resource to test with
+resource "null_resource" "test" {
+  triggers = {
+    always_run = timestamp()
+  }
+  
+  provisioner "local-exec" {
+    command = "echo 'Terraform executed via Atlantis!'"
+  }
+}
+
+output "test_message" {
+  value = "Terraform with Atlantis is working!"
+}
